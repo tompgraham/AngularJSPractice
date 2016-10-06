8932cb9a5f1336cd3352df4bcb8fa52acb397b45
tompgraham <tgraha03@uw.edu>
2016-10-05 19:13:14 -0700

    Initial Commit

---
+++ TomGrahamMP1/public_html/src/ButtonHandlers.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+"use strict";
+
+var myModule = angular.module("MyApp", []);
+
+myModule.controller('myCtrl', function($scope)
+{
+    $scope.parties = 
+    [
+        {model : "Democratic"},
+        {model : "Republican"},
+        {model : "Libertarian"}
+    ];
+});
